746af900744c53d3e97fed24f882b5fede1367f8
StefanGary <stefanfgary@gmail.com>
2025-02-18 20:19:47 +0000

    Add start workflow paramters list and update Conda path

--- dvcgit.sh
+++ dvcgit.sh
@@ -10,8 +10,7 @@ dvc_dir="$3"
 env_name="$4"
 
 # Start Conda and select env (you will need to cross-check the paths and env name)
-
-source ~/pw/.miniconda3c/etc/profile.d/conda.sh
+source ~/pw/software/.miniconda3c/etc/profile.d/conda.sh
 conda activate "$env_name"
 
 cd "$dvc_dir"
